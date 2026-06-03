82986bda9b2fd209560f84b7e91772a59332d84b
loties <alexislaubert@yahoo.fr>
2026-06-03 15:33:37 +0200

    verif valeurs avant la boucle

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 #include <stdlib.h>
-
+ 
 /**
  * delete_dnodeint_at_index - Delete a node at a specific index from a list
  *
@@ -11,35 +11,37 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *tmp;
-	unsigned int p = 0;
-
+	dlistint_t *current;
+	unsigned int p;
+ 
 	if (head == NULL || *head == NULL)
-		return (-1);
-
-	tmp = *head;
-	while (p < index && tmp != NULL)
 	{
-		tmp = tmp->next;
-		p++;
-	}
-
-	if (tmp == NULL)
 		return (-1);
-
+	}
+	current = *head;
 	if (index == 0)
 	{
-		*head = tmp->next;
-		if (*head != NULL)
-			(*head)->prev = NULL;
+		*head = current->next;
+		if (current->next != NULL)
+		{
+			current->next->prev = NULL;
+		}
+		free(current);
+		return (1);
 	}
-	else
+	for (p = 0; p < index && current != NULL; p++)
 	{
-		tmp->prev->next = tmp->next;
-		if (tmp->next != NULL)
-			tmp->next->prev = tmp->prev;
+		current = current->next;
 	}
-
-	free(tmp);
+	if (current == NULL)
+	{
+		return (-1);
+	}
+	current->prev->next = current->next;
+	if (current->next != NULL)
+	{
+		current->next->prev = current->prev;
+	}
+	free(current);
 	return (1);
 }
